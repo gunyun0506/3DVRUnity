2cc58b7f94dccf22b81d1ac2741b0e95b364892c
윤건 <gunyun050506@naver.com>
2025-07-18 17:32:34 +0900

    first commit
    frist commit.

---
+++ Assets/VRMPAssets/Scripts/Gameplay/MoveScene.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+public class MoveScene : MonoBehaviour
+{
+    public String sceneName;
+    public void MoveToScene()
+    {
+        if (string.IsNullOrEmpty(sceneName))
+        {
+            Debug.LogError("Scene name is not set.");
+            return;
+        }
+        try
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError("Error occurred while loading scene: " + ex.Message);
+        }
+    }
+
+}
